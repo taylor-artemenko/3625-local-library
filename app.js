63d959f23792d704566cc7c0d6e5fea2a1f05669
Taylor Artemenko <taylorartemenko6@gmail.com>
2020-11-25 17:48:56 -0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var compression = require('compression');
 var helmet = require('helmet');
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://tartemenko:Donkey6123@cluster0.gbunt.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://tartemenko:Donkey6123@cluster0.gbunt.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
